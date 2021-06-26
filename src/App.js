aa0ad2837c8f18ec71a84a2f6f96c17385aa53f1
19smabtahinoor <abtahinoorkabid@gmail.com>
2021-06-27 04:40:09 +0600

    Version 1.0 Display Messeges

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("")
+  const [messeges, setMessege] = useState([])
+
+  const sendMessege = (event) => {
+    setMessege([...messeges, input])
+    setInput("")
+  }
+
   return (
     <div className="App">
-     
+        <input
+          type="text"
+          placeholder="Write Messege"
+          value={input}
+          onChange={event => setInput(event.target.value)}
+        />
+        <button type="button" onClick={() => sendMessege()}>Send Messege</button>
+
+      {/* Display Messeges */}
+      <div>
+        {
+          messeges.map(messege => (
+            <h3>{messege}</h3>
+          ))
+        }
+      </div>
     </div>
   );
 }

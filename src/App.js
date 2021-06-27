214bf2d77b2e43d77fa8a3069f06b94793894233
19smabtahinoor <abtahinoorkabid@gmail.com>
2021-06-28 02:48:13 +0600

    Messeging System Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState,useEffect } from 'react';
 import Messege from './components/Messege';
 import db from './firebase/firebase';
 import {Button} from '@material-ui/core'
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
 
 function App() {
   const [input, setInput] = useState("")
@@ -10,7 +12,9 @@ function App() {
   const time = new Date().toLocaleTimeString()
 
   useEffect( () => {
-    db.collection('messeges').onSnapshot( snapshot => {
+    db.collection('messeges')
+    .orderBy('timestamp','desc')
+    .onSnapshot( snapshot => {
       setMessege(snapshot.docs.map( doc => doc.data()))
     })
   },[])
@@ -23,11 +27,13 @@ function App() {
 
     db.collection('messeges').add( {
       messege:input,
-      username:userName
+      username:userName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
     setMessege([...messeges, {username:userName,messege:input,messegeTime:time}])
     setInput("")
   }
+
 
   return (
     <div className="max-w-screen-lg mx-auto">
@@ -42,15 +48,15 @@ function App() {
         <Button disabled={!input} className="bg-blue-500 rounded-lg shadow-lg p-5" type="button" onClick={() => sendMessege()}>Send Messege</Button>
 
       {/* Display Messeges */}
-      <div>
+      <FlipMove>
         {
           messeges.map((messege,index) => (
             // <Messege key={index} text={messege.text} username={messege.username} time={messege.messegeTime}/>
 
-            <Messege username={userName} messege={messege}/>
+            <Messege key={index} username={userName} messege={messege}/>
           ))
         }
-      </div>
+      </FlipMove>
     </div>
   );
 }
--- src/components/Messege.js
+++ src/components/Messege.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React,{forwardRef} from 'react'
 import styled from 'styled-components'
 
 
 
-function Messege({ messege, username }) {
+const Messege = forwardRef(({ key ,messege, username },ref) => {
     const isUser = username === messege.username
 
     return (
         <MessegeWrapper>
-            <div className={`messege_guest ${isUser && 'messege_user'}`}>
-                <h3 classname={isUser ? 'messege_guest' : 'messege_user'}>{messege.username} : {messege.messege}</h3>
+            <div ref={ref} className={`messege_guest ${isUser && 'messege_user'}`}>
+                <h3 className={isUser ? 'messege_user' : 'messege_guest'}>{messege.username} : {messege.messege}</h3>
             </div>
         </MessegeWrapper>
     ) 
 
-}
+})
 
     const MessegeWrapper = styled.div`
         .messege_guest{
             background:#ddd;
             color:black;
             border-radius: 10px;
-            padding:20px;
+            padding:8px 10px;
             width:fit-content;
             font-weight:500;
             margin-top:10px;
@@ -29,7 +29,7 @@ function Messege({ messege, username }) {
         .messege_user{
             background:#477DE4;
             border-radius: 10px;
-            padding:20px;
+            padding:8px 10px;
             width:fit-content;
             font-weight:500;
             margin-left: auto;
